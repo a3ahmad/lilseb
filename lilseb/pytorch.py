b9a7dfae3230a9f11beb9c4abb9ae1aae7e26417
Anis Ahmad <a3ahmad@gmail.com>
2021-01-04 21:14:12 -0800

    lint fixes per flake8

--- lilseb/pytorch.py
+++ lilseb/pytorch.py
@@ -4,43 +4,53 @@ from torch.nn.common_types import _size_1_t, _size_2_t, _size_3_t
 
 from .algebra import Metric
 
+
 class GPLinear(nn.Module):
     def __init__(
-            self, 
+            self,
             metric: Metric,
-            in_features: int, 
-            out_features: int, 
-            bias: bool = True, 
+            in_features: int,
+            out_features: int,
+            bias: bool = True,
             versor: bool = False):  # Unsupported
         super(GPLinear, self).__init__()
 
         self.metric = metric
-        self.W = nn.Parameter(torch.empty(size=(in_features, out_features, metric.basis_dim())), requires_grad=True)
-        self.b = nn.Parameter(torch.empty(size=(out_features, metric.basis_dim())), requires_grad=True) if bias else None
+        self.W = nn.Parameter(
+            torch.empty(
+                size=(in_features, out_features, metric.basis_dim())),
+            requires_grad=True)
+        self.b = nn.Parameter(
+            torch.empty(
+                size=(out_features, metric.basis_dim())),
+            requires_grad=True) if bias else None
         self.versor = versor
 
     def forward(self, x):
         result = None
         if not self.versor:
-            result = torch.einsum('ijk,bpi,poj->bok', self.metric.geometricProductTensor, x, self.W)
+            result = torch.einsum(
+                'ijk,bpi,poj->bok',
+                self.metric.geometricProductTensor, x, self.W)
             if self.bias:
                 result = result + self.b
         return result
 
+
 class GPConv1D(nn.Module):
     def __init__(
-            self, 
+            self,
             metric: Metric,
-            in_channels: int, 
-            out_channels: int, 
-            kernel_size: _size_1_t, 
-            stride: _size_1_t = 1, 
+            in_channels: int,
+            out_channels: int,
+            kernel_size: _size_1_t,
+            stride: _size_1_t = 1,
             padding: _size_1_t = 0,         # Unsupported
-            bias: bool = True, 
+            bias: bool = True,
             padding_mode: str = 'zeros',    # Unsupported
             versor: bool = False):          # Unsupported
         super(GPConv1D, self).__init__()
-        
+
         self.metric = metric
         self.stride = stride
         self.padding = padding
@@ -48,13 +58,15 @@ class GPConv1D(nn.Module):
         self.W = nn.Parameter(
             torch.empty(
                 size=(
-                    kernel_size[0], 
-                    in_channels, 
-                    out_channels, 
-                    metric.basis_dim())), 
+                    kernel_size[0],
+                    in_channels,
+                    out_channels,
+                    metric.basis_dim())),
             requires_grad=True)
         if bias:
-            self.b = nn.Parameter(torch.empty(size=(out_channels, metric.basis_dim())), requires_grad=True)
+            self.b = nn.Parameter(
+                torch.empty(size=(out_channels, metric.basis_dim())),
+                requires_grad=True)
         self.versor = versor
 
     def forward(self, x):
@@ -62,25 +74,28 @@ class GPConv1D(nn.Module):
         if not self.versor:
             # TODO: Pad
             x = x.unfold(2, self.W.shape[0], self.stride[0])
-            result = torch.einsum('ijk,bcwki,kcoj->bohk', self.metric.geometricProductTensor, x, self.W)
+            result = torch.einsum(
+                'ijk,bcwki,kcoj->bohk',
+                self.metric.geometricProductTensor, x, self.W)
             if self.b is not None:
                 result = result + self.b
         return result
 
+
 class GPConv2D(nn.Module):
     def __init__(
-            self, 
+            self,
             metric: Metric,
-            in_channels: int, 
-            out_channels: int, 
-            kernel_size: _size_2_t, 
-            stride: _size_2_t = 1, 
+            in_channels: int,
+            out_channels: int,
+            kernel_size: _size_2_t,
+            stride: _size_2_t = 1,
             padding: _size_2_t = 0,         # Unsupported
-            bias: bool = True, 
+            bias: bool = True,
             padding_mode: str = 'zeros',    # Unsupported
             versor: bool = False):          # Unsupported
         super(GPConv2D, self).__init__()
-        
+
         self.metric = metric
         self.stride = stride
         self.padding = padding
@@ -88,14 +103,16 @@ class GPConv2D(nn.Module):
         self.W = nn.Parameter(
             torch.empty(
                 size=(
-                    kernel_size[0], 
-                    kernel_size[1], 
-                    in_channels, 
-                    out_channels, 
-                    metric.basis_dim())), 
+                    kernel_size[0],
+                    kernel_size[1],
+                    in_channels,
+                    out_channels,
+                    metric.basis_dim())),
             requires_grad=True)
         if bias:
-            self.b = nn.Parameter(torch.empty(size=(out_channels, metric.basis_dim())), requires_grad=True)
+            self.b = nn.Parameter(
+                torch.empty(size=(out_channels, metric.basis_dim())),
+                requires_grad=True)
         self.versor = versor
 
     def forward(self, x):
@@ -104,25 +121,28 @@ class GPConv2D(nn.Module):
             # TODO: Pad
             x = x.unfold(2, self.W.shape[0], self.stride[0])
             x = x.unfold(3, self.W.shape[1], self.stride[1])
-            result = torch.einsum('ijk,bchwlki,lkcoj->bohwk', self.metric.geometricProductTensor, x, self.W)
+            result = torch.einsum(
+                'ijk,bchwlki,lkcoj->bohwk',
+                self.metric.geometricProductTensor, x, self.W)
             if self.b is not None:
                 result = result + self.b
         return result
 
+
 class GPConv3D(nn.Module):
     def __init__(
-            self, 
+            self,
             metric: Metric,
-            in_channels: int, 
-            out_channels: int, 
-            kernel_size: _size_3_t, 
-            stride: _size_3_t = 1, 
+            in_channels: int,
+            out_channels: int,
+            kernel_size: _size_3_t,
+            stride: _size_3_t = 1,
             padding: _size_3_t = 0,         # Unsupported
-            bias: bool = True, 
+            bias: bool = True,
             padding_mode: str = 'zeros',    # Unsupported
             versor: bool = False):          # Unsupported
         super(GPConv3D, self).__init__()
-        
+
         self.metric = metric
         self.stride = stride
         self.padding = padding
@@ -130,15 +150,17 @@ class GPConv3D(nn.Module):
         self.W = nn.Parameter(
             torch.empty(
                 size=(
-                    kernel_size[0], 
-                    kernel_size[1], 
-                    kernel_size[2], 
-                    in_channels, 
-                    out_channels, 
-                    metric.basis_dim())), 
+                    kernel_size[0],
+                    kernel_size[1],
+                    kernel_size[2],
+                    in_channels,
+                    out_channels,
+                    metric.basis_dim())),
             requires_grad=True)
         if bias:
-            self.b = nn.Parameter(torch.empty(size=(out_channels, metric.basis_dim())), requires_grad=True)
+            self.b = nn.Parameter(
+                torch.empty(size=(out_channels, metric.basis_dim())),
+                requires_grad=True)
         self.versor = versor
 
     def forward(self, x):
@@ -148,7 +170,9 @@ class GPConv3D(nn.Module):
             x = x.unfold(2, self.W.shape[0], self.stride[0])
             x = x.unfold(3, self.W.shape[1], self.stride[1])
             x = x.unfold(4, self.W.shape[2], self.stride[2])
-            result = torch.einsum('ijk,bcdhwmlki,mlkcoj->bodhwk', self.metric.geometricProductTensor, x, self.W)
+            result = torch.einsum(
+                'ijk,bcdhwmlki,mlkcoj->bodhwk',
+                self.metric.geometricProductTensor, x, self.W)
             if self.b is not None:
                 result = result + self.b
         return result
