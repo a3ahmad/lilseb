ba2373a02191364101672f2a678da6399b8a4be7
Anis Ahmad <a3ahmad@gmail.com>
2021-01-11 19:33:28 -0800

    Add additional check to reproduce the vector basis only subset of the conformal 3D multiplication table per Hildenbrand's book

--- tests/basic.py
+++ tests/basic.py
@@ -5,27 +5,79 @@ from lilseb.pytorch import *
 
 A = np.array([[0,0,0,0,-1],[0,1,0,0,0],[0,0,1,0,0],[0,0,0,1,0],[-1,0,0,0,0]])
 M = Metric(A)
-v1 = np.zeros(M.basis_dim())
-v1[0b0] = 1.0
-v2 = np.zeros(M.basis_dim())
-v2[0b1] = 1.0
-v3 = np.zeros(M.basis_dim())
-v3[0b10] = 1.0
-v5 = np.zeros(M.basis_dim())
-v5[0b10000] = 1.0
-r = geometricProduct(v2, v5, M)
+e1 = np.zeros(M.basis_dim())
+e1[0b1] = 1.0
+e2 = np.zeros(M.basis_dim())
+e2[0b10] = 1.0
+e3 = np.zeros(M.basis_dim())
+e3[0b100] = 1.0
+e4 = np.zeros(M.basis_dim())
+e4[0b1000] = 1.0
+e5 = np.zeros(M.basis_dim())
+e5[0b10000] = 1.0
+r = geometricProduct(e1, e5, M)
 indices = np.nonzero(r)[0]
 print(indices)
 print("{0:b}".format(indices[-1]))
 print(r[indices])
 
-r = np.einsum('ijk,i,j->k', M.geometricProductTensor, v2, v5)
+print("---")
+
+r = np.einsum('ijk,i,j->k', M.geometricProductTensor, e1 , e5)
 indices = np.nonzero(r)[0]
 print(indices)
 print("{0:b}".format(indices[-1]))
 print(r[indices])
+
+print("---")
 
 a = np.random.rand(3, 8, 32)
 b = np.random.rand(8, 16, 32)
 result = np.einsum('ijk,bpi,poj->bok', M.geometricProductTensor, a, b)
 print(result.shape)
+
+print("---")
+
+A = np.array([[1,0,0,0,0],[0,1,0,0,0],[0,0,1,0,0],[0,0,0,0,-1],[0,0,0,-1,0]])
+M = Metric(A)
+e1 = np.zeros(M.basis_dim())
+e1[0b1] = 1.0
+e2 = np.zeros(M.basis_dim())
+e2[0b10] = 1.0
+e3 = np.zeros(M.basis_dim())
+e3[0b100] = 1.0
+e4 = np.zeros(M.basis_dim())
+e4[0b1000] = 1.0
+e5 = np.zeros(M.basis_dim())
+e5[0b10000] = 1.0
+
+def printIndices(indices, r):
+    if len(indices) == 0:
+        return "     0"
+    label = ""
+    for idx in indices:
+        if idx == 0:
+            name = str(r[idx])
+        else:
+            name = format(idx, "05b")
+
+        if label == "":
+            if r[idx] < 0.0:
+                label = "-" + name if idx > 0 else name
+            else:
+                label = " " + name
+        else:
+            if r[idx] < 0.0:
+                label = label + " - " + name
+            else:
+                label = label + " + " + name
+        
+    return label
+
+e = [e1, e2, e3, e4, e5]
+for i in range(5):
+    for j in range(5):
+        r = np.einsum('ijk,i,j->k', M.geometricProductTensor, e[i] , e[j])
+        indices = np.nonzero(r)[0]
+        print (printIndices(indices, r) + " ", end='')
+    print("")
